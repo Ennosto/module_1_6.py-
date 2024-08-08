e04670f8f5576e250c4bf9cfb6202116f67cab2e
Ennosto <ennosto_me@mail.ru>
2024-08-08 18:14:44 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,15 @@
+my_dict = {'Екатерина': 10, 'Анна': 8, 'Елена': 9, 'Анастасия': 7}
+print(my_dict)
+print(my_dict['Екатерина'])
+print(my_dict.get('Наталья', '"Без ошибки"'))
+my_dict.update({'Наталья': 11,
+                'Нина': 9})
+a = my_dict.pop('Нина')
+print(a)
+print(my_dict)
+
+my_set = {1, 2, 2, 3, 3, 4, 5, 5, (1, 2, 3), (1, 2, 3), (2, 1, 3), True}
+print(my_set)
+my_set.add(7), ((5,6,7))
+my_set.remove(3)
+print(my_set)
